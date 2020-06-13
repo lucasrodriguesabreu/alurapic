0321183632deac5228bd7bdfb072a8c6984b11a0
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-06-13 07:54:57 -0300

    Subindo css do site

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+    description='Leão';
+    url='https://expertdigital.net/wp-content/uploads/2018/11/linkedin-logo.png';
+}
